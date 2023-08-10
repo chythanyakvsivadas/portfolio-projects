b4693082b2d2233c07ddfec3859c160b7e65b444
chythanyakvsivadas <141807172+chythanyakvsivadas@users.noreply.github.com>
2023-08-10 11:57:48 +0530

    Add files via upload

--- Coviddeaths and Vaccination.sql
+++ Coviddeaths and Vaccination.sql
@@ -120,17 +120,145 @@ order by
 
 --GLOBAL NUMBERS
 SELECT
-    location,
     date,
-    total_cases,
-    total_deaths,
-	population_density,
-    (CAST(total_cases AS float) / CAST(population_density AS float)) * 100 AS Deathpercent
+    SUM(new_cases) AS total_new_cases,
+    SUM(new_deaths) AS total_new_deaths,
+    CASE
+        WHEN SUM(new_cases) > 0 THEN (SUM(new_deaths) * 100.0 / SUM(new_cases))
+        ELSE 0
+    END AS death_percentage
 FROM
-    [  portfolio]..covid_deaths
+   [  portfolio]..covid_deaths
+WHERE 
+    continent IS NOT NULL
+GROUP BY 
+    date
+ORDER BY 
+    date;
 
+--to get the total death percent
+
+SELECT
+    
+    SUM(new_cases) AS total_new_cases,
+    SUM(new_deaths) AS total_new_deaths,
+    CASE
+        WHEN SUM(new_cases) > 0 THEN (SUM(new_deaths) * 100.0 / SUM(new_cases))
+        ELSE 0
+    END AS death_percentage
+FROM
+   [  portfolio]..covid_deaths
+WHERE 
+    continent IS NOT NULL
+--GROUP BY date
+ORDER BY 1,2;
+
+
+--covid vaccinations
+
+select *
+From [  portfolio]..covid_vaccination
+order by 3,4
+
+--Joining Covid deaths and covid vaccinations
+
+select *
+from  [  portfolio]..covid_deaths dea
+join  [  portfolio]..covid_vaccination vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+
+--Looking at the Total population vs vaccination
+
+select dea.location,dea.continent,dea.date,dea.population_density,vac.new_vaccinations
+from  [  portfolio]..covid_deaths dea
+
+join  [  portfolio]..covid_vaccination vac
+	on dea.location = vac.location
+	and dea.date = vac.date
+WHERE 
+    dea.continent IS NOT NULL
+order by 2,3
+
+--to get the total vaccination
+
+SELECT
+    dea.continent,
+	dea.location,
+    dea.date,
+    dea.population_density,
+    vac.new_vaccinations,
+    SUM(CAST(vac.new_vaccinations AS bigint)) OVER (PARTITION BY dea.location) AS total_vaccinations
+FROM
+    [  portfolio]..covid_deaths dea
+JOIN
+    [  portfolio]..covid_vaccination vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date
+WHERE 
+    dea.continent IS NOT NULL
+    AND vac.new_vaccinations IS NOT NULL
+ORDER BY 
+    2, 3;
+
+--temp table  Rollingpeoplevaccinated
+Drop table if exists #percentpeoplevaccinated
+create table #percentpeoplevaccinated
+(
+continent nvarchar(255),
+location nvarchar(255),
+date datetime,
+population numeric,
+new_vaccination numeric,
+Rollingpeoplevaccinated numeric
+)
+
+insert into #percentpeoplevaccinated
+SELECT
+    dea.continent,
+	dea.location,
+    dea.date,
+    dea.population_density,
+    vac.new_vaccinations,
+    SUM(Convert(bigint,vac.new_vaccinations)) OVER (PARTITION BY dea.location order by dea.location,dea.date) AS Rollingpeoplevaccinated
+FROM
+    [  portfolio]..covid_deaths dea
+JOIN
+    [  portfolio]..covid_vaccination vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date
 --WHERE 
---location LIKE '%India%'
-WHERE continentis not null
-ORDER BY
-     date;
+    --dea.continent IS NOT NULL
+    --AND vac.new_vaccinations IS NOT NULL
+--ORDER BY 2, 3;
+SELECT *,
+       (Rollingpeoplevaccinated * 100) / NULLIF(population, 0) AS PercentVaccinated
+FROM #percentpeoplevaccinated;
+
+
+--create a view to store for further visualizations
+
+create view percentpeoplevaccinated as 
+SELECT
+    dea.continent,
+	dea.location,
+    dea.date,
+    dea.population_density,
+    vac.new_vaccinations,
+    SUM(Convert(bigint,vac.new_vaccinations)) OVER (PARTITION BY dea.location order by dea.location,dea.date) AS Rollingpeoplevaccinated
+FROM
+    [  portfolio]..covid_deaths dea
+JOIN
+    [  portfolio]..covid_vaccination vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date
+WHERE 
+    dea.continent IS NOT NULL
+    AND vac.new_vaccinations IS NOT NULL
+--ORDER BY 2, 3;
+SELECT *,
+       (Rollingpeoplevaccinated * 100) / NULLIF(population, 0) AS PercentVaccinated
+FROM #percentpeoplevaccinated;
+
+
+
